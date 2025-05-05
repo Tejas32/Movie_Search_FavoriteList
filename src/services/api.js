cdd8df87fa7b3504da5c8a7b26af49bb1e190743
Tejas Mundhe <tdmundhe32@gmail.com>
2025-05-05 15:31:23 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "e201c833393e8e69ed92a35a5d228b29";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
